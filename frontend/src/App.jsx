ef63638e07b2e96457f5fa8100e618cfd04e19d3
ramyasunarkani <ramyakrishnasunarkani@gmail.com>
2025-07-30 09:32:40 +0530

    login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
 import SignUp from './components/Authentication/SignUp'
+import Login from './components/Authentication/Login'
 
 function App() {
 
   return (
     <Routes>
       <Route path='signup' element={<SignUp/>}/>
+      <Route path='login' element={<Login/>}/>
     </Routes>
   )
 }
--- frontend/src/Store/auth-actions.js
+++ frontend/src/Store/auth-actions.js
@@ -20,3 +20,23 @@ export const signUpUser = ({ name, email, password, navigate }) => {
     }
   };
 };
+
+export const loginUser = ({ email, password, navigate }) => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.post(
+        'http://localhost:3000/user/login',
+        {
+          email,
+          password,
+        }
+      );
+
+      alert(response.data.message || "Account created successfully!");
+      navigate('/home');
+    } catch (err) {
+      const errorMessage = err.response?.data?.message || 'Something went wrong!';
+      alert(`Error: ${errorMessage}`);
+    }
+  };
+};
