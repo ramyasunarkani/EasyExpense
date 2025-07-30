2105b529da6f46f41b7f4e242c4dd47c3ca99b73
ramyasunarkani <ramyakrishnasunarkani@gmail.com>
2025-07-30 13:03:00 +0530

    secure pass

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
 
 const signup = async (req, res) => {
   const { name, email, password } = req.body;
@@ -6,24 +7,26 @@ const signup = async (req, res) => {
   try {
     const existingUser = await User.findOne({ where: { email } });
     if (existingUser) {
-      return res.status(409).json({
-        message: 'Email already exists',
-      });
+      return res.status(409).json({ message: 'Email already exists' });
     }
+
+    const saltRounds = 10;
+    const hash = await bcrypt.hash(password, saltRounds);
 
     const newUser = await User.create({
       name,
       email,
-      password, 
+      password: hash,
     });
 
-    res.status(201).json({
+    return res.status(201).json({
       message: 'User created successfully',
       userId: newUser.id,
     });
+
   } catch (err) {
     console.error(err);
-    res.status(500).json({
+    return res.status(500).json({
       message: 'Failed to create user',
       error: err.message,
     });
@@ -35,23 +38,26 @@ const login = async (req, res) => {
 
   try {
     const existingUser = await User.findOne({ where: { email } });
+
     if (!existingUser) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: 'User does not exist' });
     }
 
-    if (existingUser.password !== password) {
-      return res.status(401).json({ message: 'Invalid password' });
+    const isMatch = await bcrypt.compare(password, existingUser.password);
+
+    if (isMatch) {
+      return res.status(200).json({ message: 'Login successfully' });
+    } else {
+      return res.status(400).json({ message: 'Incorrect password' });
     }
 
-    res.status(200).json({ message: 'User login successful' });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Login failed', error: err.message });
+    return res.status(500).json({ message: 'Login failed', error: err.message });
   }
 };
 
-
-module.exports={
-    signup,
-    login
-}
+module.exports = {
+  signup,
+  login,
+};
