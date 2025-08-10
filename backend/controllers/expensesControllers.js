84c1a6d6e6bf0dfb0e4c9bbef23b184b31859744
ramyasunarkani <ramyakrishnasunarkani@gmail.com>
2025-08-10 19:03:21 +0530

    optimisation

--- backend/controllers/expensesControllers.js
+++ backend/controllers/expensesControllers.js
@@ -1,51 +1,66 @@
-const { where } = require('sequelize');
 const expenseModel = require('../models/expenseModel');
 const { User } = require('../models');
-
+const db=require('../util/db-connection');
 const AddExpense = async (req, res) => {
-  const { category, amount, description } = req.body;
+  const transaction=await db.transaction();
+
   try {
+    const { category, amount, description } = req.body;
     const newExpense = await expenseModel.create({
       category,
       amount,
       description,
       UserId:req.user.id
-    });
+    },{transaction});
     await User.increment('totalExpenses',{
       by:amount,
-      where:{id:req.user.id}
+      where:{id:req.user.id},
+      transaction
     })
+        await transaction.commit();
+
     return res.status(200).json({
       expense: newExpense,
       message: "success"
     });
   } catch (error) {
+    await transaction.rollback();
     return res.status(500).json({ message: "Internal server error" });
   }
 };
 
 const DeleteExpense = async (req, res) => {
-  const { id } = req.params;
+    const transaction=await db.transaction();
+
   try {
+    const { id } = req.params;
     const expenseToDelete = await expenseModel.findOne({
-      where: { id, UserId: req.user.id }
+      where: { id, UserId: req.user.id },
+      transaction
     });
 
     if (!expenseToDelete) {
+      await transaction.rollback();
+
       return res.status(404).json({ message: "Expense not found" });
     }
 
     await expenseModel.destroy({
-      where: { id, UserId: req.user.id }
+      where: { id, UserId: req.user.id },
+      transaction
     });
 
     await User.decrement('totalExpenses', {
       by: expenseToDelete.amount,
-      where: { id: req.user.id }
+      where: { id: req.user.id },
+      transaction
     });
+
+    await transaction.commit();
 
     res.status(200).json({ message: "Expense deleted successfully" });
   } catch (error) {
+    await transaction.rollback();
     console.error("Error deleting expense:", error);
     res.status(500).json({ message: "Internal server error" });
   }
