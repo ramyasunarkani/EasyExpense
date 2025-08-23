2c8854db53984caa64c129db2377e9791d642678
Ramyakrishna Sunarkani <142170194+ramyasunarkani@users.noreply.github.com>
2025-08-23 11:45:22 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react()],
+  server: {
+    host: "0.0.0.0", 
+    port: 5173       
+  }
 })
