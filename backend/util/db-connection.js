ac447dcf98da4442839ea20c160582a38598ffbc
ramyasunarkani <ramyakrishnasunarkani@gmail.com>
2025-08-23 11:13:12 +0530

    check

--- backend/util/db-connection.js
+++ backend/util/db-connection.js
@@ -1,5 +1,6 @@
 require('dotenv').config({ quiet: true });
 const Sequelize = require('sequelize');
+console.log(process.env.DB_NAME,process.env.DB_USER,  process.env.DB_PASSWORD,process.env.DB_HOST,process.env.DB_DIALECT,'ggggggggggggggggggg')
 
 const connection = new Sequelize(
   process.env.DB_NAME,
